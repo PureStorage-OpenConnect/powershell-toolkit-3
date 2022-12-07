4d4760c49226abaeb75322da52ad0d93757e772f
Aleksei Guzev <aleksei@guzev.pro>
2022-12-07 12:05:32 +0300

    Refactor Set-MPIODiskLBPolicy

--- src/WinAdmin/Set-MPIODiskLBPolicy.ps1
+++ src/WinAdmin/Set-MPIODiskLBPolicy.ps1
@@ -1,3 +1,13 @@
+enum MPIODiskLBPolicy {
+    clear = 0
+    FO    = 1
+    RR    = 2
+    RRWS  = 3
+    LQD   = 4
+    WP    = 5
+    LB    = 6
+}
+
 function Set-MPIODiskLBPolicy() {
     <#
     .SYNOPSIS
@@ -30,38 +40,25 @@ function Set-MPIODiskLBPolicy() {
     #>
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory)][ValidateSet('LQD','RR','clear','FO','RRWS','WP','LB',IgnoreCase = $true)][string]$Policy
+        [Parameter(Mandatory)][MPIODiskLBPolicy]$Policy
     )
-    If ($Policy -eq "LQD") { $pn = "4" }
-    elseif ($Policy -eq "RR") { $pn = "2" }
-    elseif ($Policy -like "clear") { $pn = "0" }
-    elseif ($Policy -eq "FO") { $pn = "1" }
-    elseif ($Policy -eq "RRWS") { $pn = "3" }
-    elseif ($Policy -eq "WP") { $pn = "5" }
-    elseif ($Policy -eq "LB") { $pn = "6" }
-    else {
-        Write-Host "Required policy type parameter of LQD, RR, FO, RRWS, WP LB, or clear not supplied. Exiting."
-        break
-    }
-    function Invoke-MPclaim($param1, $param2, $param3, $param4) {
-        . mpclaim.exe $param1 $param2 $param3 $param4
-    }
+
     #Checks whether mpclaim.exe is available.
     $exists = Test-Path "$env:systemroot\System32\mpclaim.exe"
     if (-not ($exists)) {
-        Write-Host "mpclaim.exe not found. Is MultiPathIO enabled? Exiting." -ForegroundColor Yellow
-        break
+        Write-Error 'mpclaim.exe not found. Is MultiPathIO enabled? Exiting.' -ErrorAction Stop
     }
-    Write-Host "Setting MPIO Load Balancing Policy to" + $pn + " for all Pure FlashArray disks."
-    $puredisks = Get-PhysicalDisk | Where-Object FriendlyName -Match "PURE"
+
+    Write-Host "Setting MPIO Load Balancing Policy to $([int]$Policy) for all Pure FlashArray disks."
+    $puredisks = Get-PhysicalDisk | Where-Object FriendlyName -Match 'PURE'
     $puredisks | ForEach-Object {
         # Get disk uniqueid
         $UniqueID = $_.UniqueId
-        $MPIODisk = (Get-WmiObject -Namespace root\wmi -Class mpio_disk_info).driveinfo | Where-Object { $_.SerialNumber -eq $UniqueID }
-        $MPIODiskID = $MPIODisk.Name.Replace("MPIO Disk", "")
+        $MPIODisk = (Get-CimInstance -Namespace root\wmi -Class MPIO_DISK_INFO).driveinfo | Where-Object { $_.SerialNumber -eq $UniqueID }
+        $MPIODiskID = $MPIODisk.Name.Replace('MPIO Disk', '')
         $MPIODiskID
-        Invoke-mpclaim -param1 "-l" -param2 "-d" -param3 $MPIODiskID -param4 $pn
+        mpclaim.exe  -l -d $MPIODiskID [int]$Policy
     }
-    Write-Host "New disk LB policy settings:" -ForegroundColor Green
-    Invoke-mpclaim -param1 "-s" -param2 "-d" -param3 "" -param4 ""
+    Write-Host 'New disk LB policy settings:' -ForegroundColor Green
+    mpclaim.exe -s -d
 }
