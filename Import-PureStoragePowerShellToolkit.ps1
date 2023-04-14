676d68af65f51e20f1ff2e641eededed4983ccab
Aleksei Guzev <aleksei.guzev@viacode.com>
2023-04-14 12:16:34 +0300

    Merge cross-platform submodules

--- Import-PureStoragePowerShellToolkit.ps1
+++ Import-PureStoragePowerShellToolkit.ps1
@@ -1,54 +1,5 @@
-function Import-ModuleManually {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory, ValueFromPipeline)]
-        [string]$ModuleName
-    )
-
-    process {
-
-        $manifestPath = Join-Path $ModuleName ($ModuleName + '.psd1')
-        $manifest = Import-PowerShellDataFile $manifestPath
-
-        $requiredModules = $manifest.RequiredModules | Where-Object { $_ -is [hashtable] -and $_['ModuleName'] -NotLike 'PureStoragePowershellToolkit*' }
-
-        $requiredModules | ForEach-Object {
-
-            $v = $_.ModuleVersion
-            $n = $_.ModuleName
-
-            $m = Get-Module -ListAvailable $n 
-
-            if (-not $m) {
-                if ($PSCmdlet.ShouldContinue("Required module $n not found. Install it?", 'Required module installation')) {
-                    Install-Module $n -MinimumVersion $v -ErrorAction Stop
-                }
-                else {
-                    Write-Host "Required module $n not installed. Aborting."
-                    exit
-                }
-            }
-            else {
-                $actualVersion = ($m.Version | Measure-Object -Maximum).Maximum
-                if ($actualVersion -lt $v) {
-                    if ($PSCmdlet.ShouldContinue("Module $n version $actualVersion is lower than required $v. Update the module?", 'Required module update')) {
-                        Install-Module $n -RequiredVersion $v -ErrorAction Stop -Force
-                    }
-                    else {
-                        Write-Host "Required module $n not updated. Aborting."
-                        exit
-                    }
-                }
-            }
-        }
-
-        Write-host "Import $ModuleName"
-        Import-Module ".\$ModuleName"
-    }
-}
+. $PSScriptRoot\Import-PureStoragePowerShellToolkit.Core.ps1
 
 'PureStoragePowershellToolkit.WindowsAdministration',
-'PureStoragePowershellToolkit.FlashArray',
-'PureStoragePowershellToolkit.DatabaseTools',
 'PureStoragePowershellToolkit.Reporting',
 'PureStoragePowershellToolkit.Exchange' | Import-ModuleManually
